da629a1429362c81809d8ef9f237e94604f17c2e
remystreich <remystreich@gmail.com>
2024-09-28 01:11:15 +0200

    swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get the status of the API' })
+  @ApiResponse({ status: 200, description: 'API is running' })
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('/status')
+  @ApiOperation({ summary: 'Get the status of the API' })
+  @ApiResponse({ status: 200, description: 'API is running' })
   async getStatus() {
     return this.appService.getStatus();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('API Pmscan')
+    .setDescription("Documentation de l'API Pmscan")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/apidoc', app, document);
   await app.listen(3000);
 }
 bootstrap();
